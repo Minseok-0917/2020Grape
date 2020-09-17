c892f50195f83e687ae47904a83e6fe5055c2bda
Potto <37059223+Minseok-0917@users.noreply.github.com>
2020-09-17 11:02:58 +0900

    Create 321.php

---
+++ 321.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+
+require "../lib.php";
+require "../web.php";
+
+src\Core\DB::exec("INSERT INTO users SET id=?,pw=?,name=?",['admin','1234','관리자']);
+
+if( src\Core\DB::fetch("SELECT count(*) count FROM books ",[])->count  ==  0 ){
+	$f = fopen("작가와의만남.csv","r");
+	$count = 0;
+	while($row = fgetcsv($f)){
+		foreach ($row as $key => $value) {
+			$row[$key] = iconv("EUC-KR","UTF-8",$row[$key]);
+		}
+		if( $count == 2 ){
+			src\Core\DB::exec("INSERT INTO books SET title = ?,artist = ?,student = ?,date = ?,week = ?,time = ?,img = ?",[
+				$row[0],
+				$row[1],
+				$row[2],
+				$row[3],
+				$row[4],
+				$row[5],
+				$row[0].".jpg"
+			]);
+		}else{
+			$count++;
+		}
+	}
+}
+
+src\Core\Route::init();
